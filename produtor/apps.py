d971222043d65fc373acc51adc7bdf44f01ef4a0
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-11-21 12:37:22 -0300

    🎉 init: re-init nextjs
    tremorjs

--- produtor/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProdutorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'produtor'
---
+++ src/app/page.tsx
@@ -0,0 +1,57 @@
+import {
+  Card,
+  Grid,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Text,
+  Title,
+} from "@tremor/react";
+
+export default function Home() {
+  return (
+    <main className="p-12">
+      <Title>Fazenda Cristo Rei</Title>
+      <Text>Dados da fazenda - Cristo Rei</Text>
+
+      <TabGroup className="mt-6">
+        <TabList>
+          <Tab>Geral</Tab>
+          <Tab>Detalhados</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
+              <Card>
+                {/* Placeholder to set height */}
+                <div className="h-28" />
+              </Card>
+              <Card>
+                {/* Placeholder to set height */}
+                <div className="h-28" />
+              </Card>
+              <Card>
+                {/* Placeholder to set height */}
+                <div className="h-28" />
+              </Card>
+            </Grid>
+            <div className="mt-6">
+              <Card>
+                <div className="h-80" />
+              </Card>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="mt-6">
+              <Card>
+                <div className="h-96" />
+              </Card>
+            </div>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </main>
+  );
+}
